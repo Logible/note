3f9616e983ea08daeb62268abe3c31c0dc2ed8eb
Log <77790264+Logible@users.noreply.github.com>
2022-07-21 02:26:24 +0800

    20 450

--- 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
+++ 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
@@ -1,17 +1,15 @@
-LinkList List_HeadInsert(LinkList &L) //逆向建立单链表
+bool delete_s_t(Sqlist &L, ElemType s, ElemType t)
 {
-    LNode *s;
-    int x;
-    L = (LinkList)malloc(sizeof(LNode)); //创建头结点
-    L->next = NULL;                      //初始为空链表
-    scanf("%d", &x);                     //输入结点的值
-    while (x != 9999)
-    {                                       //输入 9999 表示结束
-        s = (LNode *)malloc(sizeof(LNode)); //仓U建新结点①
-        s->data = x;
-        s->next = L->next;
-        L->next = s;        //将新结点插入表中，L为头指针
-        scanf("%d", &x);
+    int i, k = 0;
+    if (L.length == 0 || s >= t)
+        return false;
+    for (i = 0; i < L.length; i++)
+    {
+        if (L.data[i] >= s && L.data[i] <= t)
+            k++;
+        else
+            L.data[i - k] = L.data[i]; //当前元素前移k个位置
     }
-    return L;
+    L.length -= ;
+    return true;
 }
