1adc41df02f55f78ff4f92d9ac3cb8ba4ced1661
Log <77790264+Logible@users.noreply.github.com>
2022-07-22 01:54:37 +0800

    21 460

--- 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
+++ 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
@@ -1,15 +1,28 @@
-bool delete_s_t(Sqlist &L, ElemType s, ElemType t)
+void SearchExchangelnsert(ElemType A[], ElemType x)
 {
-    int i, k = 0;
-    if (L.length == 0 || s >= t)
-        return false;
-    for (i = 0; i < L.length; i++)
+    int low = 0, high = n - 1, mid; // low和high指向顺序表下界和上界的下标
+    while (low <= high)
     {
-        if (L.data[i] >= s && L.data[i] <= t)
-            k++;
+        mid = (low + high) / 2; //找中间位置
+        if (A[mid] == x)
+            break; //找到 x,退出 while 循环
+        else if (A[mid] < x)
+            low = mid + 1; //到中点 mid 的右半部去査
         else
-            L.data[i - k] = L.data[i]; //当前元素前移k个位置
+            high = mid - 1; //到中点mid的左半部去查
     }
-    L.length -= ;
-    return true;
+    //下面两个if语句只会执行一个
+    if (A[mid] == x && mid != n - 1)
+    { //若最后一个元素与x相等，则不存在与其后继交换的操作
+        t = A[mid];
+        A[mid] = A[mid + 1];
+        A[mid + 1] = t;
+    }
+    if (low > high)
+    {
+        //查找失败，插入数据元素x
+        for (int i = n - 1; i > high; i--)
+            A[i + 1] = A[i]; //后移元素
+        A[i + 1] = x;        //插入 x
+    }                        //结束插入
 }
