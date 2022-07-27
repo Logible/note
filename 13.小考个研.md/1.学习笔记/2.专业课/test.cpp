5629a4a41b1fabd8f15ce89a0568f2c373483901
Log <77790264+Logible@users.noreply.github.com>
2022-07-28 01:35:06 +0800

    469

--- 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
+++ 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
@@ -1,6 +1,25 @@
-typedef struct LinkNode
+bool isEmpty(SqQueue Q)
 {
-    /* data */
-    ElemType data;         // 数据域
-    struct LinkNode *Next; //指针域
-} * LiStack;               //栈类型定义
+    if (Q.rear = Q.front)
+        return true;
+}
+
+//入队
+bool EnQueue(SqQueue &Q, ElemType x)
+{
+    if ((Q.rear + 1) % MaxSize == (Q.front + 1) % MaxSize)
+        return false; //队满, 报错
+    Q.data[Q.rear] = x;
+    Q.rear = (Q.rear + 1) % MaxSize;
+    return true;
+}
+
+//出队
+bool DeQueue(SqQueue &Q, ElemType x)
+{
+    if (Q.rear == Q.front)
+        return false; //队空, 报错
+    Q.data[Q.front] = x;
+    Q.front = (Q.front + 1) % MaxSize;
+    return true;
+}
