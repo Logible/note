66195aac3569c253f8e904df1cd9e752b5f3df57
Log <77790264+Logible@users.noreply.github.com>
2022-08-11 01:58:01 +0800

    10 500

--- 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
+++ 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
@@ -1,22 +1,5 @@
-// S是主串, T是模式串
-int Index(SString S, SString T, int next[])
+typedef struct BiTNode
 {
-    int i = 1;
-    j = 1;
-    while (i <= S.length && j <= T.length)
-    {
-        if (j == 0 || S.ch[i] == T.ch[j]) //继续比较后继字符
-        {
-            ++i;
-            ++j;
-        }
-        else
-        {
-            j = next[j];
-        }
-    }
-    if (j > T.length) //匹配成功,j=length+1;
-        return i - T.length;
-    else
-        return 0;
-}
+    ElemType data;                    //数据域
+    struct BiTNode *lchild, *rchild; //左、右孩子指针
+} BiTNode, *BiTree;
