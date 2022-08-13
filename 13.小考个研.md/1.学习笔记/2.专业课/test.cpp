818f4f6bee634106e37babf0b38429ccf361ed6c
Log <77790264+Logible@users.noreply.github.com>
2022-08-13 11:25:28 +0800

    12 500

--- 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
+++ 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
@@ -1,5 +1,43 @@
-typedef struct BiTNode
+void PostThread(ThreadTree &p, ThreadTree &pre)
 {
-    ElemType data;                    //数据域
-    struct BiTNode *lchild, *rchild; //左、右孩子指针
-} BiTNode, *BiTree;
+    if (p != NULL)
+    {
+        //处理根节点
+        visit(p, pre);
+
+        //递归 线索化左子树
+        PostThread(p->lchild, pre);
+
+        //递归 线索化左子树
+        PostThread(p->rchild, pre);
+    }
+}
+
+//处理根节点
+void visit(ThreadTree &p, ThreadTree &pre)
+{
+
+    if (p->lchild == NULL)
+    {
+        p->lchild = pre;
+        p->ltag = 1;
+    }
+
+    if (pre != NULL && pre->rchild == NULL)
+    {
+        pre->rchild = p;
+        pre->tag = 1;
+    }
+    pre = p;
+}
+
+void CreatePostThread(ThreadTree T)
+{
+    ThreadTree pre = NULL;
+    if (T != NULL) //非空二叉树, 线索化
+    {
+        PostThread(T, pre); //线索化二叉树
+        pre->rchild = NULL; //处理遍历的最后一个结点
+        pre->tag = 1;
+    }
+}
