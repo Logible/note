86acc0e4bc7c3a793951f006839a5cd620b136d0
Log <77790264+Logible@users.noreply.github.com>
2022-08-15 02:35:48 +0800

    14 490 power failure

--- 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
+++ 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
@@ -1,43 +1,32 @@
-void PostThread(ThreadTree &p, ThreadTree &pre)
+#define MAX_TREE_SIZE 100 //树中最多结点树
+
+typedef struct
 {
-    if (p != NULL)
-    {
-        //处理根节点
-        visit(p, pre);
+    ElemType data; //数据元素
+    int parent;    //双亲位置域
+} PTNode;
 
-        //递归 线索化左子树
-        PostThread(p->lchild, pre);
-
-        //递归 线索化左子树
-        PostThread(p->rchild, pre);
-    }
-}
-
-//处理根节点
-void visit(ThreadTree &p, ThreadTree &pre)
+typedef struct //树的类型定义
 {
+    PTNode nodes[MAX_TREE_SIZE]; //双亲表示
+    int n;                       //结点数
+} PTree;
 
-    if (p->lchild == NULL)
-    {
-        p->lchild = pre;
-        p->ltag = 1;
-    }
+- 拐点的必要条件
+  - 设y =f(x)在点$x_0$处可导, 且$x_0$为f(x)的拐点点,则$f'(x_0) = 0$
 
-    if (pre != NULL && pre->rchild == NULL)
-    {
-        pre->rchild = p;
-        pre->tag = 1;
-    }
-    pre = p;
-}
-
-void CreatePostThread(ThreadTree T)
-{
-    ThreadTree pre = NULL;
-    if (T != NULL) //非空二叉树, 线索化
-    {
-        PostThread(T, pre); //线索化二叉树
-        pre->rchild = NULL; //处理遍历的最后一个结点
-        pre->tag = 1;
-    }
-}
+1. 第一充分条件
+   1. 若$f'(x_0)=0$(或$f(x)在x_0$连续), 且在$x_0$的某去心领域$\mathring{U}(x_0, \delta)$可导
+      1. $f^{'-}(x_0)>0, f^{'+}(x_0)<0$, 极大值
+      2. $f^{'-}(x_0)<0, f^{'+}(x_0)>0$, 极小值
+      3. 不变, 无拐点
+2. 第二充分条件
+   1. $f'(x_0) = 0, f''(x_0) \neq 0$, 则f(x)在$x_0$取得拐点
+      1. $f''(x_0) > 0$, 极小值
+      1. $f''(x_0) < 0$, 极大值
+3. 第三充分条件
+   1. 若$f'(x_0) = f''(x_0) = \dots = f^{n-1}(x_0)=0, f^{n}(x_0) \neq 0$
+      1. n为偶数, 有拐点
+         1. $f^{n}(x_0) > 0$, 极小值
+         2. $f^{n}(x_0) < 0$, 极大值
+      1. n为奇数, 无拐点
