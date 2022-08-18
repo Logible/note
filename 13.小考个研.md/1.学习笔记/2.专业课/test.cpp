aa1d8c567731b1fb17067c1d97a553b5d8efc006
Log <77790264+Logible@users.noreply.github.com>
2022-08-19 02:42:00 +0800

    18 525

--- 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
+++ 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
@@ -1,5 +1,11 @@
-typedef struct CSNode
+#define MaxVertexNum 100 //顶点数目的最大值
+
+typedef char VertexType; //顶点的数据类型
+//带权图中边上权值的数据类型
+typedef int EdgeType;
+typedef struct
 {
-   ElemType data;
-   struct CSNode *firstchild, *nextsibling;
-} CSNode, *CSTree;
+   VertexType Vex[MaxVertexNum];              //顶点表
+   EdgeType Edge[MaxVertexNum][MaxVertexNum]; //邻接矩阵，边表
+   int vexnum, arcnum;                        //图的当前顶点数和弧数
+} MGraph;
