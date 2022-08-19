73526f5906a73ed49e3a134f9e89c14aa3263b9c
Log <77790264+Logible@users.noreply.github.com>
2022-08-20 02:31:37 +0800

    19 525

--- 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
+++ 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
@@ -1,11 +1,16 @@
-#define MaxVertexNum 100 //顶点数目的最大值
 
-typedef char VertexType; //顶点的数据类型
-//带权图中边上权值的数据类型
-typedef int EdgeType;
-typedef struct
+
+Elemtype com_ancester(BTree T[], int i, int j)
 {
-   VertexType Vex[MaxVertexNum];              //顶点表
-   EdgeType Edge[MaxVertexNum][MaxVertexNum]; //邻接矩阵，边表
-   int vexnum, arcnum;                        //图的当前顶点数和弧数
-} MGraph;
+   if (T[i] != '#' && T[j] != '#') //两个结点不为空
+   {
+      while (i != j)
+      {
+         if (i > j)
+            i = i / 2;
+         if (j > i)
+            j = j / 2;
+      }
+      return T[i];
+   }
+}
