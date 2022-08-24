d39cf4a25f429ac3a19a9c48b8de9e43aa82ecf4
Log <77790264+Logible@users.noreply.github.com>
2022-08-25 02:58:26 +0800

    24 520

--- 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
+++ 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
@@ -1,27 +1,25 @@
-bool visited[MAX_VERTEX_NUM]; //访问标记数组
-
-void BFSTraverse(Graph G)
+void BFS_MIN_Distance(Graph G, int v)
 {
-   //初始化以访问标记数据
-   for (int i = 0; i < G.vexnum; i++)
-      visited[i] = false;
-
-   for (int i = 0; i < G.vexnum; i++) //对每个连通分量调用一次BFS
-      if (!visited[i])
-         DFS(G, i);
-}
-
-//从顶点v出发, 深度优先遍历图G
-void DFS(Graph G, int v)
-{
-   visit(v);                                                        //访问初始顶点v
-   visited[v] = true;                                               //对v做已访问标记
-   for (w = FirstNeighbor(G, v); w >= 0; w = NextNeighbor(G, v, w)) //检测v所有邻点
+   for (i = 0; i < G.vexnum; i++)
    {
-      // w为v的尚未访问的邻接顶点
-      if (!visited[w])
+      d[i] = "∞";
+   }
+
+   d[v] = 0;
+   visited[v] = true; //对v做已访问标记
+   EnQueue(Q, v);     //顶点V入队列
+
+   while (!isEmpty(Q))
+   {
+      Dequeue(Q, v);                                                   //顶点v出队列
+      for (w = FirstNeighbor(G, v); w >= 0; w = NextNeighbor(G, v, w)) //检测v所有邻点
       {
-         DFS(G, v);
+         if (!visited[w]) // w为v的尚未访问的邻接顶点
+         {
+            d[w] = d[u] + 1;
+            visited[w] = true;
+            EnQueue(Q, W); //顶点w入队列
+         }
       }
    }
 }
