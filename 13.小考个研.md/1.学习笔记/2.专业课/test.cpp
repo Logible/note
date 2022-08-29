feec7a301a1c1192eb3ac2b86ccb7319bae23cb1
Log <77790264+Logible@users.noreply.github.com>
2022-08-30 02:14:12 +0800

    29 510

--- 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
+++ 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
@@ -7,24 +7,15 @@ typedef struct BiTNode
    struct BiTnode *lchild, *rchild;
 } BiTNode, *BTree;
 
-int maxWinth(BTree T)
+typedef struct
 {
-   p = preorder(T, 0);
-   width = max_element(p);
-}
+   ElemType *elem; //元素的存储空间基址, 建表时按实际长度分配
+   int TableLen;   //表的长度
+} SStable;
 
-int *preOrder(BTree T, int deep)
+int Search_Seq(SStable ST, ElemType key)
 {
-   static widNum[T.height];
-   if (T == NULL)
-      return;
-   else
-   {
-      widNum[deep]++;
-      if (T->lchild)
-         preOrder(T->lchild, deep + 1);
-      if (T->rchild)
-         preOrder(T->rchild, deep + 1);
-   }
-   return widNum;
+   ST.elem[0] = key; //光明哨兵
+   for (int i = ST.TableLen; ST.elem[i] != key; --i); //从后往前找
+   return i;//若表中不存在关键字为key的元素,将查找到i为0时退出for循环
 }
