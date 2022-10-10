9e8a855952f65686c6c1f02bc83d4ef0a2a3df1e
Log <77790264+Logible@users.noreply.github.com>
2022-10-10 10:11:50 +0800

    1009 470

--- 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
+++ 13.小考个研.md/1.学习笔记/2.专业课/test.cpp
@@ -1,10 +1,10 @@
 void BFS_MIN_Distance(Graph G, int v)
 {
-    //d[i]表示从u到i结点的最短路径
+   // d[i]表示从u到i结点的最短路径
    for (i = 0; i < G.vexnum; i++)
    {
       d[i] = "∞";
-      path[i]=-1;
+      path[i] = -1;
    }
 
    d[v] = 0;
@@ -19,10 +19,30 @@ void BFS_MIN_Distance(Graph G, int v)
          if (!visited[w]) // w为v的尚未访问的邻接顶点
          {
             d[w] = d[v] + 1;
-            path[w]=[u]//最短路径应从u到w
-            visited[w] = true;
+            path[w] = [u] //最短路径应从u到w
+                visited[w] = true;
             EnQueue(Q, W); //顶点w入队列
          }
+      }
+   }
+}
+
+void PreOrder2(BiTree T)
+{
+   InitStack(S);
+   BiTree p = T; // p是遍历指针
+   while (p || !IsEmpty(S))
+   { //栈不空 或 p不空时循环
+      if (p)
+      {              //一路向左
+         visit(p);   //访问出栈结点
+         Push(S, p); //当前结点入栈
+         p = p->lchild;
+      }
+      else
+      {
+         Pop(S, p);     //栈顶元素出栈
+         p = p->rchild; //向右子树走
       }
    }
 }
